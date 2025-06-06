090af7abce307f4ddff8261776cc99608df4e62e
Noah Fonseca <nfonseca@rrc.ca>
2025-06-06 15:53:46 -0500

    Fixed CSS connection:

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   has_one_attached :image
   validates :full_name, presence: true
-  validates :email_address, presence: true
+  validates :phone_number, presence: true
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
   end
