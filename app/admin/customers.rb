1cac70c390b791e19cf80339498e26e17665c10d
Noah Fonseca <nfonseca@rrc.ca>
2025-06-06 11:50:53 -0500

    Added Images

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,20 +1,29 @@
 ActiveAdmin.register Customer do
   actions :all
-  permit_params :full_name, :phone_number, :email_address, :notes
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  form do |f|
+    f.inputs "Customer Details" do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email_address
+      row :notes
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag customer.image, class: "admin-image"
+        end
+      end
+    end
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,13 @@
 class Customer < ApplicationRecord
+  has_one_attached :image
+  validates :full_name, presence: true
+  validates :email_address, presence: true
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
   end
+  def self.ransackable_associations(auth_object = nil)
+    ["image_attachment", "image_blob"]
+  end
+
+
 end
